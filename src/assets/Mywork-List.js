92940e9d55871be3df709623557c131786c06fdd
vishnu ashok <vishnuashok35@gmail.com>
2024-08-10 12:20:02 +0530

    photo-add

--- src/assets/Mywork-List.js
+++ src/assets/Mywork-List.js
@@ -3,6 +3,12 @@ import project_2 from '../assets/xiaomi.png'
 import project_3 from '../assets/portfolio2.png'
 import project_4 from '../assets/portfolio.png'
 
+import project_5 from '../assets/digital-clock.png'
+import project_6 from '../assets/e-com.png'
+import project_7 from '../assets/img-gallery.png'
+import project_8 from '../assets/note-app.png'
+import project_9 from '../assets/quiz-app.png'
+
 const Mywork_Data=[
     {
         w_no:'1',
@@ -20,9 +26,34 @@ const Mywork_Data=[
         w_img:project_3
     },
     {
-        w_no:'',
+        w_no:'4',
         w_name:'personal website using react js',
         w_img:project_4
+    },
+    {
+        w_no:'5',
+        w_name:'live time digital clock using html,css,java script',
+        w_img:project_5
+    },
+    {
+        w_no:'6',
+        w_name:'E-commerce product using html,css and javascript',
+        w_img:project_6
+    },
+    {
+        w_no:'7',
+        w_name:'Sliding image gallery using html,css and javascript',
+        w_img:project_7
+    },
+    {
+        w_no:'8',
+        w_name:'simple note app using html,css and javascript',
+        w_img:project_8
+    },
+    {
+        w_no:'9',
+        w_name:'Quiz app using htmal,css and javascript ',
+        w_img:project_9
     },
 ]
 
